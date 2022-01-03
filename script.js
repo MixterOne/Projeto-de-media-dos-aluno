659e01c34959475b24a233a0f7ef9ef21c8da4cf
MixterOne <matheusisaias879@gmail.com>
2022-01-03 19:40:13 -0300

    confirmar

--- script.js
+++ script.js
@@ -1,44 +1,7 @@
 let form = document.querySelector('.formulario');
 
 form.addEventListener('submit', function(e) {
-    
-
-    //introdução da div
-    let divNova = document.createElement("div");
-    divNova.classList.add("boxAluno");
-
-    let img = document.createElement("img");
-    img.src = "MF.png";
-
-    //let conteudoNovo = document.createTextNode("Olá, cumprimentos!");
-    //divNova.appendChild(conteudoNovo);
-
-    let divAtual = document.querySelector(".container");
-    divAtual.appendChild(divNova);
-
-    //Conteudo da div
-
-    let nomeDoAluno = document.createElement("h1")
-    nomeDoAluno.classList.add("nomeAluno")
-
-
-    let primeiraNotaDoAluno = document.createElement("p")
-    primeiraNotaDoAluno.classList.add("notaAluno1")
-
-    let segundaNotaDoAluno = document.createElement("p")
-    segundaNotaDoAluno.classList.add("notaAluno2")
-
-    let mediaDoAluno = document.createElement("p")
-    mediaDoAluno.classList.add("media")
-    
-    divNova.appendChild(img);
-    divNova.appendChild(nomeDoAluno)
-    divNova.appendChild(primeiraNotaDoAluno)
-    divNova.appendChild(segundaNotaDoAluno)
-    divNova.appendChild(mediaDoAluno)
-    
-
-    
+        
     //valor dos input
     capturandoNome = document.querySelector('.name').value;
     document.querySelector('.nomeAluno').innerHTML = capturandoNome;
@@ -56,6 +19,16 @@ form.addEventListener('submit', function(e) {
 
     let mediaNota = (n1 + n2) / 2;
     document.querySelector('.media').innerHTML = `<p>A media da nota de ${capturandoNome} é: ${mediaNota}</p>`
+
+    if(mediaNota >= 20){
+        
+        document.querySelector('.media').innerHTML =  `<p>A media da nota de ${capturandoNome} é: ${mediaNota}<br> Aluno(a) ta de parabéns </span></p>`
+        
+    }else if(mediaNota >= 15){
+        document.querySelector('.media').innerHTML = `<p>A media da nota de ${capturandoNome} é: ${mediaNota}<br> Aluno(a) Atingiu a nota necessária para passar!</p>`
+    }else if(mediaNota < 15){
+        document.querySelector('.media').innerHTML = `<p>A media da nota de ${capturandoNome} é: ${mediaNota}<br> Aluno(a) Em recuperação</p>`
+    }
 
     // impede o envio do form
     e.preventDefault();
